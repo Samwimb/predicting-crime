23126d725b308086c82102da976b84333c071a57
JStubbs <jd.stubbs.730@gmail.com>
2019-08-22 14:21:57 -0400

    justin update

--- app.py
+++ app.py
@@ -19,8 +19,12 @@ from flask import Flask, jsonify, render_template
 from flask_apscheduler import APScheduler
 from flask_sqlalchemy import SQLAlchemy
 
-import tensorflow
+import tensorflow as tf
+import keras
+from keras.utils import CustomObjectScope
+from keras.initializers import glorot_uniform
 from keras.models import load_model, Sequential
+from keras.layers import Dense, Dropout
 from sklearn.preprocessing import LabelEncoder
 
 
@@ -67,20 +71,20 @@ def getWeather():
     forecast.clear()
     forecast.append(date.today().strftime("%A, %d %B, %Y"))   # <--- USED FOR DEBUGGING
     for i, d in enumerate(data['data']):
-        entry = {'date': {
+        entry = {
                     'text': d['valid_date'],
                     'year': int(d['valid_date'].split('-')[0]),
                     'month': int(d['valid_date'].split('-')[1]),
                     'date': int(d['valid_date'].split('-')[2]),
-                    'day': getWeekday(day + i)
-                },
-                'weather': {
+                    'day': getWeekday(day + i),
                     'max_temp': d['max_temp'],
                     'min_temp': d['min_temp'],
                     'precip': d['precip'],
-                    'lunar': round(d['moon_phase'], 4)
+                    'lunar': round(d['moon_phase'], 4),
+                    'wind': d['wind_spd'],
+                    'snow': d['snow'],
+                    'snow_depth': d['snow_depth']
                 }
-            }
         forecast.append(entry)
 
 # Creates Numpy Array of data from weather forecast for crime prediction
@@ -90,15 +94,26 @@ def generateSamples(n=16):
     samples = np.array([[]])
     for i in range(n):
         d = forecast[i]
-        s = np.array([[d['date']['month'], d['date']['date'], d['date']['day'],
-                       d['weather']['max_temp'], d['weather']['min_temp'],
-                       d['weather']['precip'], d['weather']['lunar']]])
+        s = np.array([[d['wind'], d['snow'], d['snow_depth'], d['max_temp'], d['min_temp'],
+                       d['day'], d['date'], d['month'], d['year'], d['lunar']]])
         samples = np.append(samples, s, axis=0)
     return samples
 
 # Returns text labels for test sample predictions
-def predict(model, test):
-    return list(labels.inverse_transform(model.predict_classes(test)))
+def predict(models, samples):
+    prediction_list = []
+    for m in models:
+        region = {'label': m['label'],
+                  'predictions': [],
+                  'days': []
+                 }
+        for s in samples:
+            region['days'].append(getWeekday(s[5]))                                                # <-- check index for day of week
+            with graph.as_default():
+                x = labels.inverse_transform(m['model'].predict_classes(s))
+            region['predictions'].append(x)
+        prediction_list.append(region)
+    return prediction_list
 
 
 #################################################
@@ -122,13 +137,41 @@ def predict(model, test):
 # Load ML Model & Initialize Test Data
 #################################################
 
-# dc_crime_model = load_model("voice_model_trained.h5")       # <--- CHANGE FILE PATH
+global graph
+graph = tf.get_default_graph()
 
+# Load models for each region
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+        DCmodel = load_model('./static/models/alldistricts.h5')
+        D1model = load_model('./static/models/district1.h5')
+        D2model = load_model('./static/models/district2.h5')
+        D3model = load_model('./static/models/district3.h5')
+        D4model = load_model('./static/models/district4.h5')
+        D5model = load_model('./static/models/district5.h5')
+        D6model = load_model('./static/models/district6.h5')
+        D7model = load_model('./static/models/district7.h5')
+
+# Store models with label
+Models = [
+    {'label': 0, 'model': DCmodel},
+    {'label': 1, 'model': D1model},
+    {'label': 2, 'model': D2model},
+    {'label': 3, 'model': D3model},
+    {'label': 4, 'model': D4model},
+    {'label': 5, 'model': D5model},
+    {'label': 6, 'model': D6model},
+    {'label': 7, 'model': D7model}
+]
+
+# Initialize reverse encoding
 labels = LabelEncoder()
 labels.fit(['VeryLow', 'Low', 'Medium', 'High', 'VeryHigh'])
 
+# Initialize weather forecast
 getWeather()
-# testSamples = generateSamples(5)
+
+# Generate test samples for today + n-days (1 + 5)
+Samples = generateSamples(6)
 
 
 #################################################
@@ -141,16 +184,16 @@ def index():
     return render_template("justins_playground.html")
     # return render_template("index.html")        # <--- ENSURE THIS POINTS TO THE CORRECT HOMEPAGE
 
-@app.route("/get_forecast")
+@app.route("/get_weather")
 def getForecast():
     """Diagnostic Route"""
     return jsonify(forecast)
 
-@app.route("/forecast")
+@app.route("/crime_forecast")
 def crimeForecast():
     """Return array with today's crime prediction and 5-day forecast"""
-    return jsonify(list(labels.inverse_transform([0, 2, 2, 1, 4, 0])))      # <--- FOR TESTING ONLY
-    # return jsonify(predict(dc_crime_model, generateSamples(6)))
+    # return jsonify(list(labels.inverse_transform([0, 2, 2, 1, 4, 0])))      # <--- FOR TESTING ONLY
+    return jsonify(predict(Models, Samples))
 
 
 #################################################
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,19 +1,31 @@
 
 var weekDay = ['', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-var test = ['', 'Low', 'Low', 'High', 'VeryHigh', 'Medium', 'VeryLow', 'Medium']
-
 var summaryTitle = d3.select('#summary-title');
 summaryTitle.html("");
 
 var summaryBody = d3.select('#summary-body');
 summaryBody.html("");
 
+var predictions = [];
+d3.json('/crime_forecast', data => {
+    data.forEach(p => {
+        predictions.append(p);
+    });
+});
+
+var weather;
+d3.json('/get_weather', data => {
+    weather = data[0];
+});
+
 var myMap = L.map("map", {
     center: [38.9072, -77.0369],
     zoom: 11,
     // layers: [districts]
 });
+
+var geojson;
 
 function getColor (val) {
     switch (val) {
@@ -25,50 +37,79 @@ function getColor (val) {
     }
 }
 
-d3.json('/forecast', function(predictions) {
-    d3.json('/get_forecast', function(data) {
-        summaryTitle.html("<u><strong>Crime Prediction</strong></u>")
-        summaryTitle.append("h5").text(`${data[0]}`)
-        summaryTitle.append("img")
-            .attr('src', `static/images/flag_${predictions[0]}.png`)
-            .attr('class', 'mainflag')
-        
-        L.geoJSON(dcBoundary, {
-            onEachFeature: function (feature, layer) {
-                layer.bindPopup("<h3>" + feature.properties.NAME + "</h3><hr><p>" + 
-                    feature.properties.NBH_NAMES + "</p>");                               // <--- update with crime prediction info?
-            },
-            style: {
-                color: getColor(predictions[0]),
-                fill: false,
-                weight: 6
-            }
-        }).addTo(myMap);
+function style(feature) {
+    if (feature.properties.DISTRICT == 0) {
+        return {
+            color: getColor(predictions.filter(d => { d.label == feature.properties.DISTRICT}).predictions[0]),
+            weight: 6,
+            fill: false
+        }
+    }
+	return {
+		fillColor: getColor(predictions.filter(d => { d.label == feature.properties.DISTRICT}).predictions[0]),
+		weight: 2,
+		color: 'black'
+	};
+}
 
-        predictions.forEach((p, i) => {
-            if (i == 0) { return; }
-            summaryBody.append("tr")
-            summaryBody.append("td").text(`${weekDay[data[i+1].date.day]}:`)
-                .attr('style', 'text-align:right; width:40%;')
-            summaryBody.append("td").append("img")
-                .attr('src', `static/images/flag_${p}.png`)
-                .attr('class', 'smallflag')
-        })
+function highlightFeature(e) {
+	var layer = e.target;
+
+    layer.setStyle({
+		weight: 5,
+		color: fillColor
+	});
+
+	if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+		layer.bringToFront();
+    }
+    
+    buildPanel(layer.feature);
+}
+
+function resetHighlight(e) {
+	geojson.resetStyle(e.target);
+}
+
+function zoomToFeature(e) {
+	map.fitBounds(e.target.getBounds());
+}
+
+function onEachFeature(feature, layer) {
+    // layer.bindPopup("<h3>" + feature.properties.NAME + "</h3>");       // <--- update with crime prediction info?
+
+	layer.on({
+		mouseover: highlightFeature,
+		mouseout: resetHighlight,
+		click: zoomToFeature
+	});
+}
+
+function buildPanel(feature) {
+    summaryTitle.html("<u><strong>Crime Prediction</strong></u>")
+    summaryTitle.append("h5").text(`${feature.properties.NAME}`)
+    summaryTitle.append("h5").text(`${weather}`)
+
+    r = predictions.filter(d => { d.label == feature.properties.DISTRICT });
+
+    summaryTitle.append("img")
+        .attr('src', `static/images/flag_${r.predictions[0]}.png`)
+        .attr('class', 'mainflag')
+
+    r.predictions.forEach( (d, i) => {
+        if ( i == 0 ) { return; }
+        summaryBody.append("tr")
+        summaryBody.append("td").text(`${weekDay[r.days[i]]}:`)
+            .attr('style', 'text-align:right; width:40%;')
+        summaryBody.append("td").append("img")
+            .attr('src', `static/images/flag_${d}.png`)
+            .attr('class', 'smallflag')
     })
-});
+}
 
-// var districts = L.geoJSON(districtsData, {
-//     onEachFeature: function (feature, layer) {
-//         layer.bindPopup("<h3>" + feature.properties.NAME + "</h3><hr><p>" + feature.properties.NBH_NAMES + "</p>");
-//     },
-//     style: function (feature) {
-//         return {
-//             color: '#000000',
-//             width: 2,
-//             fillColor: getColor(test[feature.properties.DISTRICT])
-//         };
-//     }
-// });
+geojson = L.geoJSON(boundaries, {style: style, onEachFeature: onEachFeature}).addTo(myMap);
+
+buildPanel(boundaries.features[0]);
 
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -76,3 +117,41 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: "mapbox.streets",
     accessToken: "pk.eyJ1IjoiZGMtY3JpbWUtYXBwIiwiYSI6ImNqeWF0eGxjZTAyYzAzbXFtbjloaG9yYWIifQ.fO2HGOd4tD6oI7JTwHQRZw"
 }).addTo(myMap);
+
+
+
+
+
+
+
+
+
+// d3.json('/crime_forecast', function(predictions) {
+//     d3.json('/get_weather', function(weather) {
+        
+
+//         p = predictions[0];
+//         summaryTitle.append("img")
+//             .attr('src', `static/images/flag_${p.predictions[0]}.png`)
+//             .attr('class', 'mainflag')
+
+//         L.geoJSON(boundaries, {
+//             onEachFeature: onEachFeature,
+//             style: {
+//                 color: getColor(predictions.filter(d => { d.label == feature.properties.DISTRICT}).predictions[0]),
+//                 fill: false,
+//                 weight: 6
+//             }
+//         }).addTo(myMap);
+        
+//         p.predictions.forEach((d, i) => {
+//             if (i == 0) {return;}
+//             summaryBody.append("tr")
+//             summaryBody.append("td").text(`${weekDay[p.days[i]]}:`)
+//                 .attr('style', 'text-align:right; width:40%;')
+//             summaryBody.append("td").append("img")
+//                 .attr('src', `static/images/flag_${d}.png`)
+//                 .attr('class', 'smallflag')
+//         })        
+//     })
+// });
