62742020478048cde99e74e6f2c8f7c671b199fa
JStubbs <jd.stubbs.730@gmail.com>
2019-08-23 20:01:15 -0400

    update relative file paths for heroku...one more time

--- DC_Crime_Predicted/static/js/logic.js
+++ DC_Crime_Predicted/static/js/logic.js
@@ -110,9 +110,7 @@ d3.json('/get_weather', data => {
 
             // Add large flag for today's prediction
             summaryTitle.append("img")
-                .attr('src', d3.json(`/getIMG/${r.predictions[0]}`, function(x) { 
-                    console.log(x.path);
-                    return x.path; }))
+                .attr('src', `/app/DC_Crime_Predicted/static/images/flag_${r.predictions[0]}`)
                 .attr('class', 'mainflag')
 
             // Add day of the week and small flag for the rest of the days in the prediction (default=5)
@@ -122,7 +120,7 @@ d3.json('/get_weather', data => {
                 summaryBody.append("td").text(`${weekDay[r.days[i]]}:`)
                     .attr('style', 'text-align:right; width:40%;')
                 summaryBody.append("td").append("img")
-                    .attr('src', d3.json(`/getIMG/${d}`, function(x) { return x.path; }))
+                    .attr('src', `/app/DC_Crime_Predicted/static/images/flag_${d}`)
                     .attr('class', 'smallflag')
             })
         }
